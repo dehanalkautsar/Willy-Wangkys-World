359f290fcf5bbc5017bf102b0d509d59ba51ce5d
darubagus <darubagus@gmail.com>
2020-11-02 13:09:51 +0700

    point sm jam tp blm fix

---
+++ Point/point.c
@@ -0,0 +1,7 @@
+#include "point.h"
+
+// Make Wahana
+//koordinatWahana *makeKoordinatWahana(wahana *wahana, int row, int kol);
+
+//print Wahana
+void printWahana(koordinatWahana P);
---
+++ Point/point.h
@@ -0,0 +1,27 @@
+#ifndef point_h
+#define point_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "boolean.h"
+
+typedef struct {
+    //wahana *wahana;
+    int X; /* absis   */
+    int Y; /* ordinat */
+} koordinatWahana;
+
+/* *** Notasi Akses: Selektor POINT *** */
+#define Kol(P) (P).X
+#define Brs(P) (P).Y
+//#define wahana(C) (P).wahana
+
+// Make Wahana
+//koordinatWahana *makeKoordinatWahana(wahana *wahana, int row, int kol);
+
+//print Wahana
+void printWahana(koordinatWahana P);
+
+
+#endif /* point_h */
